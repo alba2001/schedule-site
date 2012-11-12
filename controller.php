2f8e972caee3200b97c1622a5e62a67917a7a1ba
Ovcharenko Konstantin <alba2001@meta.ua>
2012-11-12 15:45:14 +0200

    Добавлена автоматическая отправка на e-mail уведомления о предстоящем ч-з 10 дней дне рождения. (4ч.)

--- controller.php
+++ controller.php
@@ -42,7 +42,8 @@ class ScheduleController extends JController
             exit;
 	}
 	/**
-	 * cancel editing a record
+	 * Автоматическое заполнение календаря занятий и
+         * отправка писем о предстоящем дне роджения преподавателя
 	 * @return void
 	 */
 	function fill_calendar()
@@ -53,17 +54,58 @@ class ScheduleController extends JController
                     . DS . 'com_schedule'
                     . DS . 'models'
                     . DS . 'training.php');
+            require_once(JPATH_ROOT
+                    . DS . 'administrator'
+                    . DS . 'components'
+                    . DS . 'com_schedule'
+                    . DS . 'models'
+                    . DS . 'trainers.php');
 
-//		$model = $this->getModel('training');
+            $mod_trainers = new SchedulesModelTrainers;
+            $trainers = $mod_trainers->soon_birth_day('10', TRUE);
+            // Отправка письма со списком преподавателей
+            if($trainers)
+            {
+                echo $this->_send_email($trainers)->message.'<br/>';
+            }
+            
+            // у которых день рожденья ч-з 10 дней.
             $model = new SchedulesModelTraining;
+            $msg = '';
 
 		if ($model->auto_store_calendar(TRUE)) {
-			$msg = JText::_( 'Calendar filled!' );
+			$msg .= '<br/>'.JText::_( 'Calendar filled!' );
 		} else {
-			$msg = JText::_( 'Error Filling Calendar' );
+			$msg .= '<br/>'.JText::_( 'Error Filling Calendar' );
 		}
                 echo $msg;exit;
 	}
+        /**
+         * Отправка писм преподаватлям
+         * @param array $trainers - список преподавателей.
+         * @return bolean
+         */
+        private function _send_email($body)
+        {
+            $config =& JFactory::getConfig();
+//            global $mainframe;
+            $email['from'] = array( 
+                $config->getValue( 'config.mailfrom' ),
+                $config->getValue( 'config.fromname' ) );
+            $params = &JComponentHelper::getParams('com_schedule');
+            $email['to'] = $params->get('email');
+            $email['replay'] = $config->getValue( 'config.mailfrom' );
+            $email['subject'] = JTEXT::_('Anahata trainers birthday');
+            $email['body'] = implode('</br>', $body);
+            $mailer = & JFactory::getMailer();
+            $mailer->setSender($email['from']);
+            $mailer->addRecipient($email['to']);
+            $mailer->addReplyTo($email['replay']);
+            $mailer->setSubject($email['subject']);
+            $mailer->setBody($email['body']);
+            $mailer->IsHTML(true);
+            return $mailer->Send();
+        }
 
 }
 ?>
